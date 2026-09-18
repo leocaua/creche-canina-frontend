49e928fb71a5ccbeb29829f62b1f95df9445cc33
Leonardo Cauã <leonardocauagsilva@icloud.com>
2026-09-18 01:50:00 -0300

    chore: atualiza rotas globais, configuracoes do app e estilos base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,41 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import path from 'node:path';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { MeusPets } from './pages/meus-pets/meus-pets';
+import { MatriculasComponent } from './pages/matriculas/matriculas';
+import { Rotina } from './pages/rotina/rotina';
+import { PainelAdmin } from './pages/painel-admin/painel-admin';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+
+  {
+    path: 'home',
+    component: Home
+  },
+
+  {
+    path:'meus-pets',
+    component: MeusPets
+  },
+
+  {
+    path: 'matriculas',
+    component: MatriculasComponent
+  },
+
+  {
+    path: 'rotina',
+    component: Rotina
+  },
+
+  {
+    path: 'painel-admin',
+    component: PainelAdmin
   },
 
   {
